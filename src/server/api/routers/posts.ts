3fb2b4c2f592a1e59700a9f443510c58beb37da0
Paul Moore <williampaulmoore@gmail.com>
2023-04-26 20:39:25 +0100

    feat: add rate limiting

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,6 +5,8 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
 
 const filterUserForClient = (user: User) => {
     return {
@@ -15,6 +17,12 @@ const filterUserForClient = (user: User) => {
         profilePicture: user.profileImageUrl
     };
 }
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -55,6 +63,12 @@ export const postsRouter = createTRPCRouter({
       content: z.string().min(1).max(255),
   })).mutation(async ({ctx, input}) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+      if(!success) {
+          throw new TRPCError({ code: "TOO_MANY_REQUESTS"  });
+      }
+
       const post = await ctx.prisma.post.create({
           data: {
             authorId,
